fdf8a52115289a4ddc7f147ebb42dc240f3b340d
RyanRiz <ryangrizky92@gmail.com>
2026-05-09 21:06:28 +0800

    feat: implement ctrlBackspace functionality for enhanced text manipulation

--- src/editor/core/event/handlers/keydown/index.ts
+++ src/editor/core/event/handlers/keydown/index.ts
@@ -4,6 +4,7 @@ import { isApple } from '../../../../utils/ua'
 import { isMod } from '../../../../utils/hotkey'
 import { CanvasEvent } from '../../CanvasEvent'
 import { backspace } from './backspace'
+import { ctrlBackspace } from './ctrlBackspace'
 import { del } from './delete'
 import { enter } from './enter'
 import { left } from './left'
@@ -17,7 +18,9 @@ export function keydown(evt: KeyboardEvent, host: CanvasEvent) {
   if (host.isComposing) return
   const draw = host.getDraw()
   // 键盘事件逻辑分发
-  if (evt.key === KeyMap.Backspace) {
+  if (isMod(evt) && evt.key === KeyMap.Backspace) {
+    ctrlBackspace(evt, host)
+  } else if (evt.key === KeyMap.Backspace) {
     backspace(evt, host)
   } else if (evt.key === KeyMap.Delete) {
     del(evt, host)
